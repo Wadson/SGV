1e468b334db7807dd1ce168f5c10b40f34e99737
Wadson <wadsonwadson@hotmail.com>
2026-01-26 19:05:05 -0300

    Tela de Cadastro de Clinte pronta Layout ok

--- SGVendas/obj/Debug/net10.0/SGVendas.AssemblyInfo.cs
+++ SGVendas/obj/Debug/net10.0/SGVendas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGVendas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd9d40ad522b4c0dbfde2d3cda8801757475f2c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d8366b51e2d10f2d0b0eb756f1459c0b60c7f4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGVendas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGVendas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
